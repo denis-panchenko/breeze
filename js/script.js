5f66e9c3a072898fc20e15ff3cc009c2272c4eb8
denis-panchenko <denpanchenko8@gmail.com>
2019-11-21 21:58:27 +0200

    add dots

--- js/script.js
+++ js/script.js
@@ -30,6 +30,10 @@ $(function () {
 
 $(document).ready(function(){
   $('.slider').slick({
-   
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
   });
 });
